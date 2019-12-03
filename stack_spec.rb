93a49c940d45d6842f6c5462c952e5e880fde8fa
Elukoye <lukoyedith@gmail.com>
2019-12-03 15:14:37 +0300

    Ruby Stack Rspec Test
    Rspec test for the Ruby Stack Implementation

---
+++ stack_spec.rb
@@ -0,0 +1,22 @@
+# rspec test for ruby_stack.rb
+require_relative 'ruby_stack.rb'
+
+describe '#push' do
+   stack = Stack.new
+   num = 5
+  it 'adds element to the stack' do
+    expect(stack.push(num)).to eq([5])
+  end
+
+  it 'removes the most recent element from the stack' do
+    expect(stack.pop).to eq(num)
+  end
+  
+  it 'returns the topmost element of the stack' do
+    expect(stack.peek).to eq(nil)
+  end 
+
+  it 'returns true if stack is empty' do
+    expect(stack.is_empty?).to eq(true)
+  end
+end
