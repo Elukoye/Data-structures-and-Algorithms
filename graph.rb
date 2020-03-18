215ab30c1d575abc61d9a59b80f43487800da6a8
Elukoye <lukoyedith@gmail.com>
2020-03-18 15:31:48 +0300

    aded weights to graph example

--- graph.rb
+++ graph.rb
@@ -20,3 +20,24 @@ end
 mary = Person.new("Mary")
 mary.add_friend("Henry")
 puts mary
+
+class City
+    attr_accessor :name, :routes
+    def initialize(name)
+        @name = name
+        @routes = {}
+    end
+
+    def add_route(city,price)
+        @routes[city] = price
+    end
+end
+
+nairobi = City.new('Nairobi')
+lagos = City.new('Lagos')
+nairobi.add_route('lagos',230)
+lagos.add_route('nairobi',150)
+pp nairobi
+pp lagos
+# pp nairobi.add_route('lagos' , $213)
+# pp lagos.add_route('nairobi', $250)
